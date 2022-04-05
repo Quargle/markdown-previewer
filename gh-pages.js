98e288a996fe0d56cc1e3211f7adb847f486ea47
Quargle <ph31pe@gmail.com>
2022-04-05 15:46:03 +0100

    update

--- gh-pages.js
+++ gh-pages.js
@@ -4,12 +4,11 @@ ghpages.publish(
 	'public', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'https://github.com/Quargle/markdown-previewer', // Update to point to your repository
+		repo: 'https://github.com/Quargle/markdown-previewer.git', // Update to point to your repository
 		user: {
-			name: 'Peter', // update to use your name
+			name: 'Peter Evans', // update to use your name
 			email: 'ph31pe@gmail.com' // Update to use your email
-		},
-		dotfiles: true
+		}
 	},
 	() => {
 		console.log('Deploy Complete!');
